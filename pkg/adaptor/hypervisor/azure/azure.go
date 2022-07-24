982ba2f51ce01ff0e08e0447804101dd81c8b836
Pradipta Banerjee <pradipta.banerjee@gmail.com>
2022-07-24 06:33:52 +0000

    Add initial skeleton for Azure provider
    Signed-off-by: Pradipta Banerjee <pradipta.banerjee@gmail.com>

---
+++ pkg/adaptor/hypervisor/azure/azure.go
@@ -0,0 +1,28 @@
+package azure
+
+import (
+	"context"
+	"github.com/Azure/azure-sdk-for-go/sdk/azcore"
+	"github.com/Azure/azure-sdk-for-go/sdk/azidentity"
+	"github.com/Azure/azure-sdk-for-go/sdk/resourcemanager/compute/armcompute"
+)
+
+
+func CreateInstance(c context.Context, cred azcore.TokenCredential, input *armcompute.VirtualMachine) error {
+	return nil
+}
+
+func DeleteInstance(c context.Context, cred azcore.TokenCredential, id string) error {
+	return nil
+}
+
+func NewAzureClient(cloudCfg Config) (azcore.TokenCredential, error) {
+
+	cred, err := azidentity.NewClientSecretCredential(cloudCfg.TenantId, cloudCfg.ClientId, cloudCfg.ClientSecret, nil)
+	if err != nil {
+		return nil, err
+	}
+	//azidentity.ClientSecretCredential
+	return cred, nil
+
+}
