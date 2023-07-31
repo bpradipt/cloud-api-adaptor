c48e7e3981132dfdb1af8e76037940aaea3e80fd
Pradipta Banerjee <pradipta.banerjee@gmail.com>
2023-07-31 21:13:14 +0530

    aws: Add custom tags to instances
    Fixes: #1280
    Signed-off-by: Pradipta Banerjee <pradipta.banerjee@gmail.com>

--- pkg/adaptor/cloud/aws/types.go
+++ pkg/adaptor/cloud/aws/types.go
@@ -4,6 +4,8 @@
 package aws
 
 import (
+	"errors"
+	"fmt"
 	"strings"
 
 	"github.com/confidential-containers/cloud-api-adaptor/pkg/adaptor/cloud"
@@ -36,6 +38,40 @@ func (i *instanceTypes) Set(value string) error {
 	return nil
 }
 
+// keyValueFlag represents a flag of key-value pairs
+type keyValueFlag map[string]string
+
+// String returns the string representation of the keyValueFlag
+func (k *keyValueFlag) String() string {
+	var pairs []string
+	for key, value := range *k {
+		pairs = append(pairs, fmt.Sprintf("%s=%s", key, value))
+	}
+	return strings.Join(pairs, ", ")
+}
+
+// Set parses the input string and sets the keyValueFlag value
+func (k *keyValueFlag) Set(value string) error {
+	// Check if keyValueFlag is initialized. If not initialize it
+	if *k == nil {
+		*k = make(keyValueFlag, 0)
+	}
+	pairs := strings.Split(value, ",")
+	for _, pair := range pairs {
+		keyValue := strings.SplitN(pair, "=", 2)
+		if len(keyValue) != 2 {
+			return errors.New("invalid key-value pair: " + pair)
+		}
+		key := strings.TrimSpace(keyValue[0])
+		value := strings.TrimSpace(keyValue[1])
+		// Append the key, value to the map
+		(*k)[key] = value
+
+	}
+
+	return nil
+}
+
 type Config struct {
 	AccessKeyId          string
 	SecretKey            string
@@ -50,6 +86,7 @@ type Config struct {
 	UseLaunchTemplate    bool
 	InstanceTypes        instanceTypes
 	InstanceTypeSpecList []cloud.InstanceTypeSpec
+	Tags                 keyValueFlag
 }
 
 func (c Config) Redact() Config {
