bc8bee9f3a9e8b28064ab8bda64ecd289817731c
Pradipta Banerjee <pradipta.banerjee@gmail.com>
2023-09-13 10:53:54 -0400

    aws: fetch metadata from emtadata service
    Also add registry auth

--- cmd/process-user-data/types.go
+++ cmd/process-user-data/types.go
@@ -16,3 +16,15 @@ type Config struct {
 	userData             string
 	userDataFetchTimeout int
 }
+
+type Endpoints struct {
+	Allowed []string `toml:"allowed"`
+}
+
+type AgentConfig struct {
+	EnableSignatureVerification bool      `toml:"enable_signature_verification"`
+	ServerAddr                  string    `toml:"server_addr"`
+	AaKbcParams                 string    `toml:"aa_kbc_params"`
+	ImageRegistryAuthFile       string    `toml:"image_registry_auth_file"`
+	Endpoints                   Endpoints `toml:"endpoints"`
+}
