d38f3d995348ea23ad4932c782ba23f2ea148d78
Pradipta Banerjee <pradipta.banerjee@gmail.com>
2024-10-02 19:12:00 +0530

    caa: Add support for handling GPU resources
    Signed-off-by: Pradipta Banerjee <pradipta.banerjee@gmail.com>

--- src/cloud-providers/azure/provider.go
+++ src/cloud-providers/azure/provider.go
@@ -341,8 +341,8 @@ func (p *azureProvider) updateInstanceSizeSpecList() error {
 		}
 	}
 
-	// Sort the InstanceSizeSpecList by Memory and update the serviceConfig
-	p.serviceConfig.InstanceSizeSpecList = provider.SortInstanceTypesOnMemory(instanceSizeSpecList)
+	// Sort the InstanceSizeSpecList and update the serviceConfig
+	p.serviceConfig.InstanceSizeSpecList = provider.SortInstanceTypesOnResources(instanceSizeSpecList)
 	logger.Printf("instanceSizeSpecList (%v)", p.serviceConfig.InstanceSizeSpecList)
 	return nil
 }
